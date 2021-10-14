10996808434fa34266218b5a3cdcd49c1ffe479c
solbin <solbin0027@naver.com>
2021-10-14 23:46:28 +0900

    BOJ DFS

---
+++ 20211014/BOJ_11724.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11724 {
+    static int N;
+    static int M;
+    static int[][] check;
+    static boolean[] checked;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        check = new int[1001][1001];
+        checked = new boolean[1001];
+
+        //check 채우기
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            check[x][y] = check[y][x] = 1;
+        }
+
+        int cnt = 0;
+        for(int i=1; i<=N; i++) {
+            if (!checked[i]) {
+                dfs(i);
+                ++cnt;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    private static void dfs(int i) {
+        checked[i] = true;
+
+        for(int j=1; j<=N; j++){
+            if(check[i][j]==1 && checked[j]==false)
+                dfs(j);
+        }
+    }
+}
